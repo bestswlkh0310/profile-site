4122f450d3e142338425eec5d2e8e2a7855bbb30
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-02-07 19:03:12 +0900

    to react site

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <div className="w-screen h-screen flex justify-center bg-[url('/img/universe.jpg')]">
+            <div className={'flex w-2/3 min-w-screen-lg h-full flex-col items-center justify-center'}>
+                <h1 className="text-3xl font-semibold align text-white">
+                    반갑다
+                </h1>
+                
+            </div>
+        </div>
+    );
+}
+
+export default App;
